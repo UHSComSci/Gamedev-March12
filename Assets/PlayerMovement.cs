91c15e6d881564e825b36543edc138e6a5aa2dff
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-02-26 16:21:51 -0500

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 10;
+    public float jump = 100;
+
+    public Transform feetTransform;
+
+    public LayerMask groundLayers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void Jump()
+    {
+        if(OnFloor())
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+    }
+
+    bool OnFloor()
+    {
+        Collider2D groundCheck =  Physics2D.OverlapCircle(feetTransform.position, 0.2f, groundLayers);
+        if(groundCheck != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    //Called once per physics frame
+    private void FixedUpdate()
+    {
+        //A: go left : -X
+        //D: go right: +X
+        //Space: Jump: +Y
+
+        // from -1 to 7
+        float move = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+
+        //float jump = Input.GetAxisRaw("Jump");
+        
+        bool jump = Input.GetButtonDown("Jump");
+
+        if (jump)
+        {
+            Jump();
+        }
+    }
+}
